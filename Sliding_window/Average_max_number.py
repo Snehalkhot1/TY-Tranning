a115dd878489debdebb1771aced99ae01bac74b9
Snehal Tanaji Khot <khot6834@gmail.com>
2026-08-12 00:38:15 +0530

    Add files via upload

---
+++ Sliding_window/Average_max_number.py
@@ -0,0 +1,15 @@
+def sliding_window(users, days):
+    window_sum = sum(users[:days])
+    max_sum = window_sum
+    for i in range(1, len(users) - days + 1):
+        window_sum = window_sum - users[i - 1] + users[i + days - 1]
+        if window_sum > max_sum:
+            max_sum = window_sum
+    return max_sum
+users = [100, 48, 87, 52, 67, 80, 90]
+days = 3
+max_sum = sliding_window(users, days)
+print("Maximum sum of sub-array:", max_sum)
+average = max_sum / days
+
+print("Average:", average)
